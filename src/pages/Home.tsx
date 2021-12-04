a0b099d3b9f4caedac71484085dfb4c43453063b
filipefilpe <filpess@hotmail.com>
2021-12-04 16:07:36 -0300

    Resolução do desafio.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,25 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks([...tasks, newTask])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const newTasks: Task[] = tasks.map(task => ({
+      ...task,
+      done: task.id === id ? !task.done : task.done
+    }))
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const newTasks: Task[] = tasks.filter(task => task.id !== id)
+    setTasks(newTasks)
   }
 
   return (
